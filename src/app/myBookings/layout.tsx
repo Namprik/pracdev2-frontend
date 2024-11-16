7e0a79e69e6e51ec6749bffedab16e05ce4eacb2
Namprik <wareesa1847@gmail.com>
2024-11-17 01:36:59 +0700

    add user.name for top menu

--- src/app/myBookings/layout.tsx
+++ src/app/myBookings/layout.tsx
@@ -1,14 +1,17 @@
+"use client";
+
 import TopMenu from "@/components/TopMenu/TopMenu";
+import { useSession } from "next-auth/react";
 
 export default function myBookingLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const name = "Naphat Wareesawetsuwan";
+  const { data: session } = useSession();
   return (
     <div className="bg-white w-screen h-screen min-w-fit min-h-fit flex flex-col">
-      <TopMenu focus="My Bookings" username={name} />
+      <TopMenu focus="My Bookings" name={session?.user.name || ""} />
       <div className="grow p-7">{children}</div>
     </div>
   );
