60cdab8e49ece0ee434f6484bf178acaa67a8015
Namprik <wareesa1847@gmail.com>
2024-11-16 19:08:09 +0700

    add focus on top menu

--- src/app/companies/layout.tsx
+++ src/app/companies/layout.tsx
@@ -5,9 +5,10 @@ export default function CompanyLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const name = "Naphat Wareesawetsuwan";
   return (
     <div className="bg-white w-screen h-screen min-w-fit min-h-fit flex flex-col">
-      <TopMenu />
+      <TopMenu focus="Companies" username={name} />
       <div className="grow p-7">{children}</div>
     </div>
   );
--- src/components/TopMenu/TopMenu.tsx
+++ src/components/TopMenu/TopMenu.tsx
@@ -1,29 +1,38 @@
 import Link from "next/link";
 import { Icon } from "@iconify/react";
 
-export default function TopMenu() {
-  const name = "Naphat Wareesawetsuwan";
+export type UserPage = "Companies" | "My Bookings";
+export default function TopMenu({
+  username,
+  focus,
+}: {
+  username: string;
+  focus: UserPage;
+}) {
+  const navigationLinks = [
+    { name: "Companies", href: "/companies" },
+    { name: "My Bookings", href: "/myBookings" },
+  ];
 
   return (
     <div className="bg-dp-navbar flex flex-row justify-between text-white p-7 items-center text-nowrap truncate space-x-10 w-full">
       <div className="flex flex-row space-x-20 items-center ">
         <h1 className="font-extrabold text-[40px]">JOB FAIR</h1>
-        <Link
-          href="/companies"
-          className="text-xl hover:font-bold focus:font-bold"
-        >
-          Home
-        </Link>
-        <Link
-          href="/myBookings"
-          className="text-xl hover:font-bold focus:font-bold"
-        >
-          My Bookings
-        </Link>
+        {navigationLinks.map((link, index) => (
+          <Link
+            key={index}
+            href={link.href}
+            className={`text-xl hover:font-bold focus:font-bold ${
+              focus == link.name ? "font-bold" : "font-normal"
+            } `}
+          >
+            {link.name}
+          </Link>
+        ))}
       </div>
       <div className="flex flex-row space-x-5 items-center">
         <label className="font-semibold text-xl text-nowrap truncate">
-          Hello, <label className="font-normal">{name}</label>
+          Hello, <label className="font-normal">{username}</label>
         </label>
         <Link href="/signIn">
           <Icon icon="mdi:logout" className="text-white size-7" />
