de94d6d1f666dada480b3a0fefc72bfe343a49e9
Namprik <wareesa1847@gmail.com>
2024-11-17 22:47:11 +0700

    fix: top menu

--- src/app/myBookings/layout.tsx
+++ src/app/myBookings/layout.tsx
@@ -1,17 +1,13 @@
-"use client";
-
 import TopMenu from "@/components/TopMenu/TopMenu";
-import { useSession } from "next-auth/react";
 
 export default function myBookingLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { data: session } = useSession();
   return (
     <div className="bg-white w-screen h-screen min-w-fit min-h-fit flex flex-col">
-      <TopMenu focus="My Bookings" name={session?.user.name || ""} />
+      <TopMenu focus="My Bookings" />
       <div className="grow p-7">{children}</div>
     </div>
   );
--- src/components/TopMenu/TopMenu.tsx
+++ src/components/TopMenu/TopMenu.tsx
@@ -1,45 +1,58 @@
-import Link from "next/link";
+"use client";
+
 import { Icon } from "@iconify/react";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
+import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 export type UserPage = "Companies" | "My Bookings";
 
-export default function TopMenu({
-  focus,
-  name,
-}: {
-  focus: UserPage;
-  name: string;
-}) {
+export default function TopMenu({ focus }: { focus: UserPage }) {
+  const router = useRouter();
+  const { data: session } = useSession();
+
   const navigationLinks = [
     { name: "Companies", href: "/companies" },
     { name: "My Bookings", href: "/myBookings" },
   ];
 
+  const handleSignOut = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+
+    try {
+      await signOut({ redirect: false });
+      router.push("/signIn");
+    } catch (error) {
+      console.error("Error during sign-out:", error);
+    }
+  };
+
   return (
     <div className="bg-dp-navbar flex flex-row justify-between text-white p-7 items-center text-nowrap truncate space-x-10 w-full">
-      <div className="flex flex-row space-x-20 items-center ">
+      <div className="flex flex-row text-white no-underline space-x-20 items-center ">
         <h1 className="font-extrabold text-[40px]">JOB FAIR</h1>
         {navigationLinks.map((link, index) => (
-          <Link
+          <button
             key={index}
-            href={link.href}
-            className={`text-xl hover:font-bold focus:font-bold ${
+            onClick={() => router.push(link.href)}
+            className={`text-xl no-underline text-white hover:font-bold focus:font-bold ${
               focus == link.name ? "font-bold" : "font-normal"
             } `}
           >
             {link.name}
-          </Link>
+          </button>
         ))}
       </div>
       <div className="flex flex-row space-x-5 items-center">
         <label className="font-semibold text-xl text-nowrap truncate">
-          Hello, <label className="font-normal">{name}</label>
+          Hello,{" "}
+          {session && (
+            <label className="font-normal">{session.user.name}</label>
+          )}
         </label>
-        <Link href="/signIn">
-          <Icon icon="mdi:logout" className="text-white size-7" />
-        </Link>
+        <button onClick={handleSignOut}>
+          <Icon icon="mdi:logout" className="text-white text-2xl" />
+        </button>
       </div>
     </div>
   );
