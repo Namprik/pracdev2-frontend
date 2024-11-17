cbf583c2f55b854e6bb27241b18cefaed1e2e9f5
Namprik <wareesa1847@gmail.com>
2024-11-17 22:47:32 +0700

    style: button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -17,7 +17,7 @@ export default function Button({
           ? "bg-dp-blue hover:bg-dp-blue-hover text-white font-bold"
           : btnType === "cancel"
           ? "bg-white border border-dp-border hover:border-dp-blue text-dp-blue"
-          : "bg-dp-red hover:border-dp-red-hover text-white font-bold"
+          : "bg-dp-red hover:bg-dp-red-hover text-white font-bold"
       } rounded-lg p-2 w-full h-[48px]`}
       type={type === "submit" ? "submit" : "button"}
     >
