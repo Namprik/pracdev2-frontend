9210505b42d2091b74e30955ea31359b159d553d
Namprik <wareesa1847@gmail.com>
2024-11-17 22:48:37 +0700

    fix: top menu

--- src/app/companies/layout.tsx
+++ src/app/companies/layout.tsx
@@ -1,16 +1,13 @@
-"use client";
 import TopMenu from "@/components/TopMenu/TopMenu";
-import { useSession } from "next-auth/react";
 
-export default function CompanyLayout({
+export default async function CompanyLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { data: session } = useSession();
   return (
     <div className="bg-white w-screen h-screen min-w-fit min-h-fit flex flex-col">
-      <TopMenu focus="Companies" name={session?.user.name || ""} />
+      <TopMenu focus="Companies" />
       <div className="grow p-7">{children}</div>
     </div>
   );
